ff57e097a6b42c98403b12ba97cfd128cf7f147c
Miguel <miguelantonio.otoya@unmsm.edu.pe>
2023-05-26 14:26:54 -0500

    primer commit

---
+++ src/redes_trabajos.js
@@ -0,0 +1,79 @@
+const REDES_SOCIALES = {
+  github: 'https://github.com/miguel1808',
+  linkedin: 'https://www.linkedin.com/in/miguel-antonio-otoya-chaname/',
+  correo: 'antoniomiguel116@gmail.com'
+}
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+const TRABAJOS = [
+  {
+    nombre: 'Encriptador de texto',
+    fecha: '28 - Oct - 2022',
+    descripcion:
+      'Encriptador basico de texto',
+    website: 'https://miguel1808.github.io/EncriptadorTexto_MiguelOtoya_Alura/',
+    github: 'https://github.com/miguel1808/EncriptadorTexto_MiguelOtoya_Alura',
+    imagen: './assets/proyectos/encriptador.png'
+  },
+  
+   
+  {
+    nombre: 'Modelos de inteligencia artificial',
+    fecha: '16 - Nov - 2022',
+    descripcion:
+      'Pagina web donde se muestran los modelos de inteligencia artificial de prediccion de datos que se han realizado',
+    website: 'https://miguel1808-eqa-proyectofinal-streamlit-app-j2zal7.streamlit.app/',
+    github: 'https://github.com/miguel1808/EqA-ProyectoFinal',
+    imagen: './assets/proyectos/ia.png'
+  },
+  {
+    nombre: 'Pagina de apeperia',
+    fecha: '03 - Dec - 2022',
+    descripcion:
+      'Pagina web de una empresa de desarrollo web',
+    website: 'https://miguel1808.github.io/curso_layout_mobile/',
+    github: 'https://github.com/miguel1808/curso_layout_mobile',
+    imagen: './assets/proyectos/apeperia.png'
+  },
+  {
+    nombre: 'Registro de tareas',
+    fecha: '10 - Dec - 2022',
+    descripcion:
+      'Pagina web donde podras registrar tus tareas y eliminarlas',
+    website: 'https://miguel1808.github.io/JS_Almacenando_Datos_En_El_Navegador/',
+    github: 'Registro de tareas',
+    imagen: './assets/proyectos/tareas.png'
+  },
+  {
+    nombre: 'Portfolio Web',
+    fecha: '25 - May - 2023',
+    descripcion:
+      'Portal web donde se muestran los proyectos realizados por mi y toda mi informacion.',
+    website: 'https://miguel1808.github.io/Portfolio',
+    github: 'https://github.com/miguel1808/Portafolio',
+    imagen: './assets/proyectos/portafolio.png'
+  },
+  {
+    nombre: 'Pagina de noticias de juegos',
+    fecha: '15 - Dec - 2022',
+    descripcion:
+      'Pagina de noticias de juegos',
+    website: 'https://miguel1808.github.io/grid_oracle/',
+    github: 'https://github.com/miguel1808/grid_oracle',
+    imagen: './assets/proyectos/noticias.png'
+  },
+  {
+    nombre: 'Juego del ahorcado',
+    fecha: '3 - Jan - 2023',
+    descripcion:
+      'Este proyecto simula el clasico juego del ahorcado donde puedes agregar un diccionario de palabras',
+    website: 'https://miguel1808.github.io/Juego-del-Ahorcado/',
+    github: 'https://github.com/jaenfigueroa/PokeZone',
+    imagen: './assets/proyectos/ahorcado.png'
+  }
+]
+
+///////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
